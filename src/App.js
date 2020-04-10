72ef73ccc33461771ea68ecacaa14551d84a07f0
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-04-09 23:33:06 -0500

    Proyecto Terminado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import Axios from 'axios';
+import Listado from './components/Listado';
 
 function App() {
+
+  const [busqueda, setBusqueda] = useState('');
+
+  const [imagenes, setImagenes] = useState([]);
+
+  const [paginaActual, setPaginaActual] = useState(1);
+
+  const [totalPaginas, setTotalPaginas] = useState(1);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (busqueda === '') return;
+
+      const imagenesPorPagina = 30;
+      const key = '15960504-57997715d9ee5c66860a91c17';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaActual}&lang=es`;
+
+      const respuesta = await Axios.get(url);
+
+      setImagenes(respuesta.data.hits);
+
+      setTotalPaginas(Math.ceil(respuesta.data.totalHits / imagenesPorPagina));
+
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior: 'smooth' })
+    }
+    consultarAPI();
+  }, [busqueda, paginaActual]);
+
+  const paginaAnterior = () => {
+
+    if (paginaActual > 1) {
+      setPaginaActual(paginaActual - 1);
+    }
+
+  }
+
+  const paginaSiguiente = () => {
+    if (paginaActual < totalPaginas) {
+      setPaginaActual(paginaActual + 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4 text-center">Buscador de imagenes</h1>
+
+        <Formulario
+          setBusqueda={setBusqueda}
+        />
+      </div>
+      <div className="row justify-content-center">
+        <Listado
+          imagenes={imagenes}
+        />
+
+        {
+          (paginaActual > 1)
+            ? <button
+              type="button"
+              className="btn btn-primary mr-1"
+              onClick={paginaAnterior}
+            >&laquo; Anterior
+            </button>
+            : null
+        }
+        {
+          (paginaActual < totalPaginas)
+            ? <button
+              type="button"
+              className="btn btn-primary"
+              onClick={paginaSiguiente}
+            >
+              Siguiente &raquo;
+          </button>
+            : null
+        }
+
+
+      </div>
     </div>
   );
 }
